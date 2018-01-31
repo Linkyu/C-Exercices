b87dd16b6ec3dabd275bb5d5d2fc3833ed347568
Kévin Guiraud <kevin.guiraud@gtd.eu>
2018-01-31 11:15:42 +0100

    Exercice 1

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApplication1
+{
+    
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Exercice1();
+        }
+
+        private static void Exercice1()
+        {
+            var numbers = new List<int>();
+
+            DisplayCommands();
+
+            var input = Console.ReadLine();
+            while (input != "*" && numbers.Count <= 20)
+            {
+                var result = int.TryParse(input, out var number);
+                if (result)
+                {
+                    numbers.Add(number);
+                }
+                else
+                {
+                    switch (input)
+                    {
+                        case "+":
+                            numbers.Sort();
+                            DisplayList(numbers);
+                            break;
+                        case "-":
+                            numbers = numbers.OrderByDescending(i => i).ToList();
+                            DisplayList(numbers);
+                            break;
+                        case "=":
+                            Console.WriteLine(numbers.Sum());
+                            break;
+                        default:
+                            Console.Error.WriteLine("Invalid input!");
+                            break;
+                    }
+                }
+
+                if (numbers.Count <= 20)
+                {
+                    input = Console.ReadLine();
+                }
+            }
+        }
+
+        private static void DisplayCommands()
+        {
+            Console.WriteLine("Please input your 20 or less numbers.");
+            Console.WriteLine("Commands are:");
+            Console.WriteLine("    + : Sort the list, then display it");
+            Console.WriteLine("    - : Sort the list in descending order, then display it");
+            Console.WriteLine("    = : Display a sum of the list");
+            Console.WriteLine("    * : Quit");
+        }
+
+        private static void DisplayList(List<int> list)
+        {
+            Console.WriteLine(string.Join(", ", list.ToArray()));
+        }
+    }
+}
